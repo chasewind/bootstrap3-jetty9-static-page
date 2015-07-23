d351b7326c58fc723e482cf67d01e7839cd3e741
yudongwei <yudw@kunion.com.cn>
2015-07-23 10:17:28 +0800

    static page

---
+++ src/test/java/com/belief/core/Bootstrap3StaticJetty.java
@@ -0,0 +1,31 @@
+package com.belief.core;
+
+
+import java.util.Arrays;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 静态页面访问，跟随着更多的注解内容，该类不可用
+ * 
+ * @author 于东伟
+ *
+ */
+public class Bootstrap3StaticJetty {
+	private static Logger logger = LoggerFactory.getLogger(Bootstrap3StaticJetty.class);
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+		WebAppContext webapp = new WebAppContext();
+		webapp.setResourceBase("src/main/webapp");
+		webapp.setContextPath("/");
+		webapp.setOverrideDescriptors(Arrays.asList(new String[] { Bootstrap3StaticJetty.class.getResource("/") + "web_override_development.xml" }));
+		server.setHandler(webapp);
+		logger.info("start......");
+		server.start();
+		server.join();
+	}
+}
